5a7c7e66a29a0be92a5e6d2dd678da351eea171e
Rafał Gierczak <gierczak.raf@gmail.com>
2019-06-05 19:12:21 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { FirstComponent } from './pages/first/first.component';
+import { SecondComponent } from './pages/second/second.component';
+import { ThirdComponent } from './pages/third/third.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'first'
+  },
+  {
+    path: 'first',
+    component: FirstComponent
+  },
+  {
+    path: 'second',
+    component: SecondComponent
+  },
+  {
+    path: 'third',
+    component: ThirdComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'first'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FirstComponent } from './pages/first/first.component';
+import { SecondComponent } from './pages/second/second.component';
+import { ThirdComponent } from './pages/third/third.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
