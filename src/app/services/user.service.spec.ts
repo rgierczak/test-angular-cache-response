bba448c8c5bb009578faece8f86bbc12c129d171
Rafał Gierczak <gierczak.raf@gmail.com>
2019-06-07 17:03:03 +0200

    Add fetchUser function unit test

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,17 +1,53 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 
-import { UserService } from './user.service';
+import { UserResponse, UserService } from './user.service';
 
 describe('UserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      HttpClientTestingModule
-    ]
-  }));
+  let userService: UserService = null;
+  let httpTestingController: HttpTestingController = null;
 
-  it('should be created', () => {
-    const service: UserService = TestBed.get(UserService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        UserService
+      ]
+    });
+
+    userService = TestBed.get(UserService);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should retrieve user data after calling fetchUser function', () => {
+    const USER_URL = 'https://jsonplaceholder.typicode.com/todos/1';
+    const REQUEST_METHOD = 'GET';
+
+    const mockUserResponse: UserResponse = {
+      id: 123,
+      title: 'test',
+      userId: 123,
+      completed: true
+    };
+
+    userService.fetchUser().then((user) => {
+      expect(user).toEqual(mockUserResponse);
+    });
+
+    const req = httpTestingController.expectOne(USER_URL);
+
+    expect(req.request.method).toEqual(REQUEST_METHOD);
+
+    req.flush(mockUserResponse);
+  });
+
 });
